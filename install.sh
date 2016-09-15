0ef1b09e0d0c79fedeab09e9a801b75f5699ead5
Adam Horrigan <adamhorrigan@Adams-MacBook-Pro.local>
2016-09-15 14:56:01 +0100

    OS environment

---
+++ install.sh
@@ -0,0 +1,50 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brews = (
+	bash-completion
+	mysql
+	youtube-dl
+	node
+	git
+	speedtest-cli
+)
+
+casks = (
+	sequel-pro
+	postman
+	vlc
+	atom
+	qbittorrent
+	slack
+	google-chrome
+	skype
+	webstorm
+)
+
+git config --global alias.lol log --oneline --graph --decorate --all
+git config --global alias.st status
+git config --global alias.com checkout master
+cp .gitignore_global ~/
+git config --global core.excludesfile ~/.gitignore_global
+
+brew tap caskroom/cask
+brew tap caskroom/versions
+
+for i in “${(brews[@]}”
+do
+	:
+	brew install $i
+done
+
+for i in “${(casks[@]}”
+do
+	:
+	brew cask install $i
+done
+
+brew cleanup
+brew cask cleanup
+
+sudo easy_install pip
+suso pip install virtualenv
+
